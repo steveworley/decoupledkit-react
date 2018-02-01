4ae9cee5b7bf068b28ca867aafc68119bc6317d5
Steve Worley <steven.worley@acquia.com>
2018-02-01 16:26:56 +1000

    GraphQL multi
    - Sets up the graphql multi app.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,11 +3,13 @@ import { combineReducers } from 'redux';
 import drupal_api_load from './drupalLoadReducer';
 import drupal_api_load_img from './drupalLoadImgReducer';
 import drupal_api_update from './drupalUpdateReducer';
+import graphqlMultiReducer from './graphqlMulti'
 
 const rootReducer = combineReducers({
   drupal_api_load,
   drupal_api_load_img,
-  drupal_api_update
+  drupal_api_update,
+  graphqlMultiReducer
 });
 
 export default rootReducer;
