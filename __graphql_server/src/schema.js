891991e4c35a33a1bce759fab269e795c3d34554
Steve Worley <steven.worley@acquia.com>
2018-02-13 15:29:00 +1000

    Add api data mocking to both graphql server and client app.
    - Adds mocking to the graphql server.
    - Adds the mocking component to present the information.

--- __graphql_server/src/schema.js
+++ __graphql_server/src/schema.js
@@ -1,10 +1,23 @@
-import { bundle } from 'graphql-modules';
-import { makeExecutableSchema } from 'graphql-tools';
+import { bundle } from 'graphql-modules'
+import { makeExecutableSchema, addMockFunctionsToSchema } from 'graphql-tools'
 
-import heroes from './types/hero';
-import villains from './types/villain';
-import comics from './types/comic';
+import heroes from './types/hero'
+import villains from './types/villain'
+import comics from './types/comic'
+import users from './types/user'
 
-const modules = [heroes, villains, comics];
+import mocks from './mocks'
 
-export default makeExecutableSchema(bundle(modules));
+const modules = [heroes, villains, comics, users]
+
+const schema = makeExecutableSchema(bundle(modules))
+
+if (process.env.MOCK) {
+  addMockFunctionsToSchema({
+    schema,
+    mocks,
+    preserveResolvers: true
+  })
+}
+
+export default schema;
