f49291ec53d1824b69773bbeea5ad4c52e06c6f6
Jason Enter <jason.enter@acquia.com>
2018-02-26 15:02:24 -0500

    Updates to include .env in webpack.

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -5,7 +5,7 @@ import ExtractTextPlugin from 'extract-text-webpack-plugin';
 import WebpackMd5Hash from 'webpack-md5-hash';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import path from 'path';
-
+const Dotenv = require('dotenv-webpack');
 const GLOBALS = {
   'process.env.NODE_ENV': JSON.stringify('production'),
   __DEV__: false
@@ -24,6 +24,9 @@ export default {
     filename: '[name].[chunkhash].js'
   },
   plugins: [
+
+    new Dotenv({ path: './.env' }), // available deployments variables in a .env file
+
     // Hash the files using MD5 so that their names change when the content changes.
     new WebpackMd5Hash(),
 
